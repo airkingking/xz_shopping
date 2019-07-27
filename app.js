27e6b7159c26abec912fda5e0767edb135747ad5
zhenghao <779240225@qq.com>
2019-07-27 08:58:59 +0800

    增加商品详情/删除/更新模块

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express=require('express');
 //此中间件虽然是对路由进行筛选拦截的，但是路由或者路由器最终是要挂在到服务器上的，
 //所以所有的中间都要写在服务器中并且要写在挂载路由之前
-const bodyparser=require('body-parser');    
+const bodyparser=require('body-parser');
+//引入同意目录下的自定义模块时'./'是必须要加的
 const user_router=require('./routes/user.js');
 const product_router=require('./routes/product.js');
 
--- routes/product.js
+++ routes/product.js
@@ -12,7 +12,7 @@ router.get('/list',function(req,res){
     pno=parseInt(pno);
     size=parseInt(size);
     var start=(pno-1)*size;
-    pool.query(`SELECT lid,price,title FROM xz_laptop LIMIT ?,?`,[start,size],function(err,result){
+    pool.query(`SELECT lid,title,price,sold_count,is_onsale FROM xz_laptop LIMIT ?,?`,[start,size],function(err,result){
         if(err) throw err;
         res.send(result);
         // 后台测试输出结果
@@ -20,9 +20,29 @@ router.get('/list',function(req,res){
     });
 });
 
-//商品详情路由
-
-//添加商品路由
+//商品详情路由  响应到浏览器的列信息需要完善
+router.get('/detail',function(req,res){
+    var obj=req.query;
+    if(!obj.lid){
+        res.send({
+            code:401,
+            msg:'lid required'
+        });
+        return;
+    }
+    pool.query(`SELECT lid,title,price FROM xz_laptop WHERE lid=?`,[obj.lid],function(err,result){
+        if(err) throw err;
+        if(result.length>0){
+            res.send(result);
+        }else{
+            res.send({
+                code:301,
+                msg:'product is not exists'
+            });
+        }
+    });
+});
+//添加商品路由  需要通过表单添加的列需要完善
 router.get('/add',function(req,res){
     var obj=req.query;
     var i=400;
@@ -52,7 +72,33 @@ router.get('/add',function(req,res){
 });
 
 //修改商品路由
-
+router.get('/update',function(req,res){
+    var obj=req.query;
+    
+});
 //删除商品路由
-
+router.get('/delete',function(req,res){
+    var obj=req.query;
+    if(!obj.lid){
+        res.send({
+            code:401,
+            msg:'lid required'
+        });
+        return;
+    }
+    pool.query(`DELETE FROM xz_laptop WHERE lid=?`,[obj.lid],function(err,result){
+        if(err) throw err;
+        if(result.affectedRows>0){
+            res.send({
+                code:200,
+                msg:'delete suc'
+            });
+        }else{
+            res.send({
+                code:301,
+                msg:'delete err'
+            });
+        }
+    });
+});
 module.exports=router;
